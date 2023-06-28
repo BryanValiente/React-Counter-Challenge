c44b99c86161032bac0ca80e08a1984d78a3f97e
TXT-33 <txt-33@TXT-33.local>
2023-06-27 20:36:12 -0700

    React counter with randomizer and some styling, would like to still add an animation or something

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,31 @@
 import { useState } from "react";
 
 const Counter = () => {
-
+    const[count, setCount] = useState(0)
+    function randomNumberInRange(min, max) {
+        // 👇️ get number between min (inclusive) and max (inclusive)
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+    const countHandler = () => {
+        setCount(count + 1)
+    };
+    const countMandler = () => {
+        setCount(count - 1)
+    };
+    const countZero = () => {
+        setCount(0)
+    };
+    const countRandom = () => {
+        setCount(randomNumberInRange(1, 100));
+        
+    };
     return(
         <div>
-            <h1></h1>
-            <button> Add </button>
-            <button> subtract </button>
-            <button> Reset </button>
+            <h1>{count}</h1>
+            <button onClick={countHandler}> Add </button>
+            <button onClick={countMandler}> subtract </button>
+            <button onClick={countZero}> Reset </button>
+            <button onClick={countRandom}> Randomizer</button>
         </div>
     )
 };
